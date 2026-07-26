c46553efe7ef2eeb3f2f63c058ee68398e2cc2a5
rudmad13 <madrigalrudy34@gmail.com>
2026-07-26 16:13:23 -0700

    Add User model and UserRepository interface

---
+++ backend/src/main/java/com/convio/backend/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.convio.backend.repository;
+
+import com.convio.backend.model.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+
+    Iterable<User> findAll();
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUniId(String id);
+    Optional<User> findById(UUID id);
+
+    boolean update(User user);
+    User save(User user);
+    boolean delete(UUID id);
+
+
+}
