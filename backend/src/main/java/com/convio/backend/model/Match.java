19fcdf79e5a7e76bca79f9ac76d07f0e7609f358
rudmad13 <madrigalrudy34@gmail.com>
2026-07-27 19:45:15 -0700

    Add Match model and MatchRepository interface

---
+++ backend/src/main/java/com/convio/backend/model/Match.java
@@ -0,0 +1,36 @@
+package com.convio.backend.model;
+
+import com.convio.backend.model.User;
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+public class Match {
+
+    private UUID id;
+    private User user1_id;
+    private User user2_id;
+    private LocalDateTime timestamp;
+
+    public Match(){};
+
+    public Match(UUID id, User user1_id, User user2_id, LocalDateTime timestamp){
+        this.id = id;
+        this.user1_id = user1_id;
+        this.user2_id = user2_id;
+        this.timestamp = timestamp;
+    }
+
+    //Getters
+    public UUID getId(){return id;}
+    public User getUser1_id(){return user1_id;}
+    public User getUser2_id(){return user2_id;}
+    public LocalDateTime getTimestamp(){return timestamp;}
+
+    //Setters
+    public void setId(UUID id){this.id = id;}
+    public void setUser1(User user){this.user1_id = user;}
+    public void setUser2(User user){this.user2_id = user;}
+    public void setTimestamp(LocalDateTime time){this.timestamp = time;}
+
+
+}
