df4dd39c505308bc721b0055533779d2f7c879a5
rudmad13 <madrigalrudy34@gmail.com>
2026-08-06 19:45:33 -0700

    Enhance JdbcUserRepositoryTest with additional user retrieval and update tests

--- backend/src/test/java/com/convio/backend/IntegrationTests/JdbcUserRepositoryTest.java
+++ backend/src/test/java/com/convio/backend/IntegrationTests/JdbcUserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.convio.backend.IntegrationTests;
 
 import java.util.UUID;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,16 +24,19 @@ public class JdbcUserRepositoryTest {
     @Autowired
     private JdbcUserRepository repository;
 
+    private User user1;
+    private User user2;
+
     @BeforeEach
     void setup(){
 
-        User user1 = new User(
+        user1 = new User(
             UUID.randomUUID(),
             "1234",
             "john@ewu.edu"
         );
 
-        User user2 = new User(
+        user2 = new User(
             UUID.randomUUID(),
             "4567",
             "apple@ewu.edu"
@@ -52,6 +56,53 @@ public class JdbcUserRepositoryTest {
 
 
     }
+
+    @Test
+    void returnUserByEmail(){
+        Optional<User> user = repository.findByEmail("john@ewu.edu");
+        assertEquals("john@ewu.edu", user.get().getUniversityId());
+
+
+    }
+
+    @Test
+    void findUserByUniversityId(){
+        Optional<User> user = repository.findByUniId("1234");
+
+        assertEquals("1234", user.get().getUniversityId());
+    }
+
+    @Test
+    void UpdateUser(){
+        user1.setUniversityEmail("changed@ewu.edu");
+
+        repository.update(user1);
+
+        Optional<User> newUser = repository.findByUniId("1234");
+
+        assertEquals(user1.getUniversityEmail(), newUser.get().getUniversityEmail());
+
+    }
+
+    @Test
+    void saveUserToDatabase(){
+        User user3 = new User(UUID.randomUUID(), "67", "newUser@ewu.edu");
+        List<User> results = repository.findAll();
+
+        repository.save(user3);
+
+        assertEquals(3, results.size());
+
+    }
     
+    @Test
+    void DeleteUser(){
+
+        List<User> results = repository.findAll();
+
+        repository.delete(user1.getId());
+
+        assertEquals(1, results.size());
+    }
 
 }
