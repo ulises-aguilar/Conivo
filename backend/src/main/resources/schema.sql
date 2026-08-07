7fb977a752f4e77924003b9c2143523500450fd7
rudmad13 <madrigalrudy34@gmail.com>
2026-08-06 20:16:44 -0700

    Fix typo in email_verified column name in users table

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     universityId VARCHAR(20) NOT NULL UNIQUE,
     universityEmail VARCHAR(20) NOT NULL UNIQUE,
-    email_verfied BOOLEAN NOT NULL
+    email_verified BOOLEAN NOT NULL
 );
